3a24a4258b73f816a36fc5f4d68f033bdf2ed64f
no-chili <18875854140@163.com>
2022-02-15 21:05:13 +0800

    new

--- src/router/router.js
+++ src/router/router.js
@@ -3,15 +3,17 @@ import Tran from '../components/Tran.vue';
 import TranGro from '../components/TranGro.vue';
 import Obscure from '../components/Obscure.vue';
 import Compont from '../components/Compont.vue';
+import Menu from '../view/Menu.vue';
 const routes = [
+    { path: '/', component: Menu },
     { path: '/tran', component: Tran },
     { path: '/trangro', component: TranGro },
-    { path: '/obscure',component:Obscure},
-    { path: '/compon',component:Compont}
-  ]
+    { path: '/obscure', component: Obscure },
+    { path: '/compon', component: Compont }
+]
 
-const router=createRouter({
-    history:createWebHistory(),
+const router = createRouter({
+    history: createWebHistory(),
     routes
 })
 
---
+++ src/view/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li><a href="/tran">Tran</a></li>
+        <li><a href="/trangro">Trangro</a></li>
+        <li><a href="/obscure">Obscure</a></li>
+        <li><a href="/compon">Compon</a></li>
+    </ul>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
